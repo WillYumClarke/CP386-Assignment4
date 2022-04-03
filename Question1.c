b517dcc5f1fcacdbb9dc99e66d8e06f4b31befbd
WilliamClarke <will.yumclarke@gmail.com>
2022-04-03 13:02:45 -0400

    troubleshooting a weird syntax error

--- Question1.c
+++ Question1.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +6,13 @@
 #include <time.h>
 #include <semaphore.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    int numCustomers; // n customers
+    int numResources; // m resource types
+    for (int i = 0; i < argc; i++){
+        printf("%s", argv[i]);
+    }
+    printf("Number of Cutsomers: ");
+    scanf("%d", &numCustomers );
+    return 0;
 }
